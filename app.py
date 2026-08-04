1e5c4a4c5a3d920943d17a3ca0cc3fa8a8b9243b
elsyrodng96-create <elsyrod96@gmail.com>
2026-08-03 22:03:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -305,7 +305,8 @@ def main():
             min_value=min_date,
             max_value=max_date,
         )
-        # Filtrar precipitación para el día seleccionado en la barra lateral
+        
+       # Filtrar precipitación para el día seleccionado en la barra lateral
         lluvia_dia = df_hist[df_hist["fecha"].dt.date == fecha_seleccionada]
         if not lluvia_dia.empty:
             val_lluvia = lluvia_dia["lluvia_mm"].values[0]
